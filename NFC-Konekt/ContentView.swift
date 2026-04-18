98fb7d2493b1484cfce8c73b0bfff764acff7d6f
Rei Soemanto <reresoemanto@gmail.com>
2026-04-18 08:12:25 +0700

    Initial Commit

---
+++ NFC-Konekt/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NFC-Konekt
+//
+//  Created by Rei Soemanto on 18/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NFC-Konekt/NFC_KonektApp.swift
@@ -0,0 +1,17 @@
+//
+//  NFC_KonektApp.swift
+//  NFC-Konekt
+//
+//  Created by Rei Soemanto on 18/04/26.
+//
+
+import SwiftUI
+
+@main
+struct NFC_KonektApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
